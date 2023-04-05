14cc23e5259c1ab57fae008e24a338ce07bb29ff
liam-kilsdonk <94444182+liam-kilsdonk@users.noreply.github.com>
2023-04-05 11:12:04 +0200

    layout media window vormgegeven en button structuur 1.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.play = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.repeat = new System.Windows.Forms.Button();
+            this.curentsong = new System.Windows.Forms.TextBox();
+            this.friendList = new System.Windows.Forms.ListBox();
+            this.playList = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(409, 419);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(94, 29);
+            this.play.TabIndex = 0;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // pause
+            // 
+            this.pause.Location = new System.Drawing.Point(309, 419);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(94, 29);
+            this.pause.TabIndex = 1;
+            this.pause.Text = "Pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // repeat
+            // 
+            this.repeat.Location = new System.Drawing.Point(509, 419);
+            this.repeat.Name = "repeat";
+            this.repeat.Size = new System.Drawing.Size(94, 29);
+            this.repeat.TabIndex = 2;
+            this.repeat.Text = "Repeat";
+            this.repeat.UseVisualStyleBackColor = true;
+            this.repeat.Click += new System.EventHandler(this.repeat_Click);
+            // 
+            // curentsong
+            // 
+            this.curentsong.Location = new System.Drawing.Point(309, 386);
+            this.curentsong.Name = "curentsong";
+            this.curentsong.Size = new System.Drawing.Size(294, 27);
+            this.curentsong.TabIndex = 3;
+            this.curentsong.TextChanged += new System.EventHandler(this.curentsong_TextChanged);
+            // 
+            // friendList
+            // 
+            this.friendList.FormattingEnabled = true;
+            this.friendList.ItemHeight = 20;
+            this.friendList.Location = new System.Drawing.Point(609, 239);
+            this.friendList.Name = "friendList";
+            this.friendList.Size = new System.Drawing.Size(150, 244);
+            this.friendList.TabIndex = 4;
+            this.friendList.SelectedIndexChanged += new System.EventHandler(this.friendList_SelectedIndexChanged);
+            // 
+            // playList
+            // 
+            this.playList.FormattingEnabled = true;
+            this.playList.ItemHeight = 20;
+            this.playList.Location = new System.Drawing.Point(153, 239);
+            this.playList.Name = "playList";
+            this.playList.Size = new System.Drawing.Size(150, 244);
+            this.playList.TabIndex = 5;
+            this.playList.SelectedIndexChanged += new System.EventHandler(this.playList_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(309, 454);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(294, 29);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1001, 595);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.playList);
+            this.Controls.Add(this.friendList);
+            this.Controls.Add(this.curentsong);
+            this.Controls.Add(this.repeat);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.play);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button play;
+        private Button pause;
+        private Button repeat;
+        private TextBox curentsong;
+        private ListBox friendList;
+        private ListBox playList;
+        private ProgressBar progressBar1;
     }
 }
