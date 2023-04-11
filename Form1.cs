9e4b46824602445e1dd006b5eedfff9497d3885c
liam-kilsdonk <94444182+liam-kilsdonk@users.noreply.github.com>
2023-04-11 09:15:13 +0200

    hulp van tijmen, tekst in listbox gekregen

--- Form1.cs
+++ Form1.cs
@@ -5,11 +5,19 @@ namespace SpotifyAppLiam
         public Form1()
         {
             InitializeComponent();
+
+            for(int i=0; i < 10; i++)
+                strings.Add(i.ToString());
+
+            strings.ForEach(str => {
+                friendList.Items.Add(str);
+            });
         }
 
         /* method initialization */
 
-        user myUser = new user();
+        user myUser = new();
+        List<string> strings = new();
 
         /* media buttons pause, play, repeat */
 
@@ -33,6 +41,7 @@ namespace SpotifyAppLiam
         private void friendList_SelectedIndexChanged(object sender, EventArgs e)
         {
             //friendlist
+
         }
 
         private void curentsong_TextChanged(object sender, EventArgs e)
